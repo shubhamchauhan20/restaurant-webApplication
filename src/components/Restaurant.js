82faf2a531a232650bf96cc56e285454d7a45b9f
Shubham Chauhan <shubhamchauhan55t@gmail.com>
2023-10-24 23:13:10 +0530

    async await added for restaurant

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
 const Restaurant = () => {
+  const [restaurants, setRestaurant]= React.useState("");
+
+  React.useState(()=>{
+    getRestaurantData()
+  },[])
+  async const getRestaurantData=()=>{
+    const data= await fetch();
+    const json= await data.json();
+    const 
+  } 
   return (
     <div className='space-y-4 rounded-lg shadow-xl w-36'>
       <img className='w-36 rounded-md h-28' src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ecbd9a23c0fdeb8efad7d5cb847ccb35' alt='img' />
